2a28c330ee0de5eead24ae2ba7c0be8e20df62e5
Zhuo Zhen <zhenz@uchicago.edu>
2019-11-21 16:21:37 -0600

    [swift] better error message when container name exists

--- openstack_dashboard/dashboards/project/static/dashboard/project/containers/containers.controller.js
+++ openstack_dashboard/dashboards/project/static/dashboard/project/containers/containers.controller.js
@@ -99,9 +99,10 @@
 
       // reverse the sense here - successful lookup == error so we reject the
       // name if we find it in swift
-      return swiftAPI.getContainer(containerName, true).then(function onSuccess(response) {
-        return $q.reject(response);
-      }, function onError() { return true; });
+      swiftAPI.getContainer(containerName, true, {redirectOnAuthErrors: false}).then(def.reject, function(response){
+    	  if (response.status == 403) {def.reject(response)}
+    	  else {def.resolve(response)}});
+      return def.promise;
     }
 
     function selectContainer(container) {
@@ -238,7 +239,7 @@
               {
                 key: 'name',
                 validationMessage: {
-                  exists: gettext('A container with that name exists.')
+                  exists: gettext('A container with that name exists. (Name must be unique within each region.)')
                 },
                 $asyncValidators: {
                   exists: checkContainerNameConflict
