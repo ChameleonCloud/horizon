2f88cba388d2551c9ea04bdbd3d7f77912268ba4
Alex Rocha <rochaa@tacc.utexas.edu>
2019-11-21 16:32:50 -0600

    setting default flavor if only one option is available

--- openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/flavor/flavor.controller.js
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/flavor/flavor.controller.js
@@ -280,6 +280,11 @@
         };
         ctrl.availableFlavorFacades.push(facade);
       }
+
+      // Set default flavor if only one available
+      if(ctrl.allocatedFlavorFacades.length < 1 && ctrl.availableFlavorFacades.length == 1){
+        ctrl.allocatedFlavorFacades.push(ctrl.availableFlavorFacades[0]);
+      }
     }
 
     /*
