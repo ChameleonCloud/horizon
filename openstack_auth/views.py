ffb5a4ac09e18f785fdf0c0e58ee9d562de9017a
Jason Anderson <796413+diurnalist@users.noreply.github.com>
2020-05-21 16:21:19 -0500

    Change Portal SSO to special case of federation (#15)
    * Rewrite custom SSO as edge case of Keystone SSO
    
    It turns out that, upon close inspection, our custom SSO flow is
    remarkably similar to the "default WebSSO redirect" flow added in the
    Stein release of Horizon. We just need to add support for one bit:
    changing the redirect mechanism to allow an operator to configure an
    absolute URL, as opposed to one created by a Keystone/IdP protocol pair.
    The rest of the flow is the same: the user is redirected to a page via a
    GET request (with the inbound origin appended as a param), and then the
    /websso endpoint in Horizon takes a POST request from the IdP with the
    Keystone token of the user, which is then used to initialize the user's
    session.
    
    This also adds support for a way to specify, even in the IdP chooser,
    that you want to use the default redirect behavior--this is accomplished
    by using "None" as the IdP in the WEBSSO_IDP_MAPPING, like so:
    
    WEBSS_IDP_MAPPING = {
      'default': (None, None),
      'some_idp': ('idp', 'saml'),
    }
    
    Instead, go back to the login page, since we didn't actually use the
    redirect flow.
    
    * Support redirect 'next' param in WebSSO
    
    If a user logs in via WebSSO, the 'next' parameter is not respected, and
    so the user ultimately must re-navigate to their target destination once
    they complete their login. This fixes that by including the 'next'
    parameter on the Keystone WebSSO start request. Keystone will ignore
    this flag. However, we can then read it from the referer when Horizon is
    handling its final part of the login flow.
    
    * Add page to display if no default logout URL
    
    If there is a default redirect URL, but no default logout URL, the user
    can effectively not logout, as they will be brought back to the login
    page, which will automatically log them in again. This adds a logout
    page with some overriddable messaging, and a link to do a "deep" logout
    
    * Support passing redirect on websso as query param
    
    Instead of extracting from the referer, sometimes the triggering service
    may choose to include this directly.

--- openstack_auth/views.py
+++ openstack_auth/views.py
@@ -77,16 +77,25 @@ def set_logout_reason(res, msg):
 @never_cache
 def login(request):
     """Logs a user in using the :class:`~openstack_auth.forms.Login` form."""
-
     # If the user enabled websso and the default redirect
     # redirect to the default websso url
-    if (request.method == 'GET' and settings.WEBSSO_ENABLED and
-            settings.WEBSSO_DEFAULT_REDIRECT):
-        protocol = settings.WEBSSO_DEFAULT_REDIRECT_PROTOCOL
-        region = settings.WEBSSO_DEFAULT_REDIRECT_REGION
+    if (request.method == 'GET' and utils.is_websso_enabled and
+            utils.is_websso_default_redirect(request)):
         origin = utils.build_absolute_uri(request, '/auth/websso/')
-        url = ('%s/auth/OS-FEDERATION/websso/%s?origin=%s' %
-               (region, protocol, origin))
+        if utils.get_websso_default_redirect_url():
+            host = request.get_host()
+            url = ('%s?origin=%s&host=%s' % (
+                utils.get_websso_default_redirect_url(), origin, host))
+        else:
+            protocol = utils.get_websso_default_redirect_protocol()
+            region = utils.get_websso_default_redirect_region()
+            url = ('%s/auth/OS-FEDERATION/websso/%s?origin=%s' %
+                       (region, protocol, origin))
+        redirect_to = request.GET.get(auth.REDIRECT_FIELD_NAME, '')
+        if redirect_to:
+            url = utils.url_query_append(url, {
+                auth.REDIRECT_FIELD_NAME: redirect_to
+            })
         return shortcuts.redirect(url)
 
     # If the user enabled websso and selects default protocol
@@ -100,6 +109,11 @@ def login(request):
             if auth_url is None:
                 auth_url = forms.get_region_endpoint(region_id)
             url = utils.get_websso_url(request, auth_url, auth_type)
+            redirect_to = request.POST.get(auth.REDIRECT_FIELD_NAME, '')
+            if redirect_to:
+                url = utils.url_query_append(url, {
+                    auth.REDIRECT_FIELD_NAME: redirect_to
+                })
             return shortcuts.redirect(url)
 
     if not request.is_ajax():
@@ -197,16 +211,19 @@ def login(request):
 @sensitive_post_parameters()
 @csrf_exempt
 @never_cache
-def websso(request):
+def websso(request, redirect_field_name=auth.REDIRECT_FIELD_NAME):
     """Logs a user in using a token from Keystone's POST."""
     referer = request.META.get('HTTP_REFERER', settings.OPENSTACK_KEYSTONE_URL)
-    auth_url = utils.clean_up_auth_url(referer)
+    if utils.is_websso_default_redirect_url(referer):
+        auth_url = settings.OPENSTACK_KEYSTONE_URL
+    else:
+        auth_url = utils.clean_up_auth_url(referer)
     token = request.POST.get('token')
     try:
         request.user = auth.authenticate(request, auth_url=auth_url,
                                          token=token)
     except exceptions.KeystoneAuthException as exc:
-        if settings.WEBSSO_DEFAULT_REDIRECT:
+        if utils.is_websso_default_redirect(request):
             res = django_http.HttpResponseRedirect(settings.LOGIN_ERROR)
         else:
             msg = 'Login failed: %s' % exc
@@ -218,7 +235,15 @@ def websso(request):
     auth.login(request, request.user)
     if request.session.test_cookie_worked():
         request.session.delete_test_cookie()
-    return django_http.HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
+    # Support passing redirect param explicitly on query params
+    redirect_to = request.GET.get(redirect_field_name)
+    if not redirect_to:
+        redirect_to = utils.url_extract_param(referer, redirect_field_name)
+    if not http.is_safe_url(url=redirect_to,
+                            allowed_hosts=[request.get_host()]):
+        redirect_to = settings.LOGIN_REDIRECT_URL
+    return django_http.HttpResponseRedirect(redirect_to)
 
 
 # TODO(stephenfin): Migrate to CBV
@@ -237,15 +262,23 @@ def logout(request, login_url=None, **kwargs):
     LOG.info(msg)
 
     """ Securely logs a user out. """
-    if (settings.WEBSSO_ENABLED and settings.WEBSSO_DEFAULT_REDIRECT and
-            settings.WEBSSO_DEFAULT_REDIRECT_LOGOUT):
-        auth_user.unset_session_user_variables(request)
-        return django_http.HttpResponseRedirect(
-            settings.WEBSSO_DEFAULT_REDIRECT_LOGOUT)
-
-    return django_auth_views.logout_then_login(request,
-                                               login_url=login_url,
-                                               **kwargs)
+    if utils.is_websso_enabled and utils.is_websso_default_redirect(request):
+        default_redirect_logout = utils.get_websso_default_redirect_logout()
+        if (default_redirect_logout and
+            not utils.get_websso_default_redirect_logout_confirm()):
+            auth_user.unset_session_user_variables(request)
+            return django_http.HttpResponseRedirect(
+                default_redirect_logout)
+        else:
+            extra_context = {'logout_url': default_redirect_logout}
+            return django_auth_views.logout(request,
+                                            template_name='auth/logout.html',
+                                            extra_context=extra_context,
+                                            **kwargs)
+    else:
+        return django_auth_views.logout_then_login(request,
+                                                   login_url=login_url,
+                                                   **kwargs)
 
 
 # TODO(stephenfin): Migrate to CBV
