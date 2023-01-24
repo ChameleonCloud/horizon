18779928d45ce557939ca28ecd1f11b3905cbab2
Adam Cooper <adamcooper@uchicago.edu>
2023-01-24 16:36:05 -0600

    Clarify comment in security groups loading code

--- openstack_dashboard/enabled/_1480_security_groups_panel.py
+++ openstack_dashboard/enabled/_1480_security_groups_panel.py
@@ -4,7 +4,7 @@ PANEL_DASHBOARD = 'project'
 PANEL_GROUP = 'network'
 PANEL = 'security_groups'
 
-# Security groups only work on KVM sites, so we only load them there
+# Security groups don't work on bare metal sites, so we don't load them there
 if not settings.CHAMELEON_IS_BAREMETAL_SITE:
     ADD_PANEL = ('openstack_dashboard.dashboards.project.security_groups'
                  '.panel.SecurityGroups')
