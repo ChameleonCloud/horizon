f238886391b6f7df1432ac6dacfff3aefce4684c
Michael Sherman <shermanm@uchicago.edu>
2024-09-22 18:00:28 -0500

    allow configuring default flavor by name
    in the instance launch dialog, allow configuration of default selected
    flavor. To do so, set `default_flavor_name` in the `LAUNCH_INSTANCE_DEFAULTS`
    configuration dict.

--- openstack_dashboard/defaults.py
+++ openstack_dashboard/defaults.py
@@ -253,6 +253,9 @@ LAUNCH_INSTANCE_DEFAULTS = {
     'enable_scheduler_hints': True,
     'enable_metadata': True,
     'enable_net_ports': True,
+    'enable_secgroups': True,
+    'enable_servergroups': True,
+    'default_flavor_name': None,
     'default_availability_zone': 'Any',
 }
 
