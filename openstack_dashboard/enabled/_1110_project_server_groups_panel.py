ff425df60741afc40888176e72e15135d33cbf27
Michael Sherman <shermanm@uchicago.edu>
2024-09-23 16:11:46 -0500

    BAREMETAL: Disable server_group and secgroup panel

--- openstack_dashboard/enabled/_1110_project_server_groups_panel.py
+++ openstack_dashboard/enabled/_1110_project_server_groups_panel.py
@@ -6,5 +6,6 @@ PANEL_DASHBOARD = 'project'
 PANEL_GROUP = 'compute'
 
 # Python panel class of the PANEL to be added.
-ADD_PANEL = ('openstack_dashboard.dashboards.project.server_groups'
-             '.panel.ServerGroups')
+# NOTE: This panel is disabled because server groups are unused
+# ADD_PANEL = ('openstack_dashboard.dashboards.project.server_groups'
+#              '.panel.ServerGroups')
