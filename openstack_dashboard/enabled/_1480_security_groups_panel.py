48a33c43aa4a2e2b6f16ad597cff5330f44504f7
Michael Sherman <shermanm@uchicago.edu>
2024-09-23 16:11:46 -0500

    BAREMETAL: Disable server_group and secgroup panel

--- openstack_dashboard/enabled/_1480_security_groups_panel.py
+++ openstack_dashboard/enabled/_1480_security_groups_panel.py
@@ -2,5 +2,6 @@ PANEL_DASHBOARD = 'project'
 PANEL_GROUP = 'network'
 PANEL = 'security_groups'
 
-ADD_PANEL = ('openstack_dashboard.dashboards.project.security_groups'
-             '.panel.SecurityGroups')
+# Security groups don't work on bare metal sites, so we don't load them there
+# ADD_PANEL = ('openstack_dashboard.dashboards.project.security_groups'
+#              '.panel.SecurityGroups')
