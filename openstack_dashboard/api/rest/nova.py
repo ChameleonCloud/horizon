c991f4c90c0850bd617418a988ed1aa78090b110
Mark Powers <markpowers@uchicago.edu>
2026-01-26 17:50:49 +0000

    Add config for blazar flavor filter

--- openstack_dashboard/api/rest/nova.py
+++ openstack_dashboard/api/rest/nova.py
@@ -542,8 +542,12 @@ class Flavors(generic.View):
                 key.startswith("resources:CUSTOM_RESERVATION_")
                 for key in d['extra_specs']
             )
-            # Include flavor if from an active reservation
-            if is_active_reservation_flavor:
+            if settings.OPENSTACK_SHOW_ONLY_RESERVED_FLAVORS:
+                # Include flavor only if from an active reservation
+                if is_active_reservation_flavor:
+                    result['items'].append(d)
+            else:
+                # Include all flavors
                 result['items'].append(d)
         return result
 
