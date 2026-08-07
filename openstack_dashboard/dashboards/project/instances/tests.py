178c6f248c127cef813b48d7a46caf9ef28c51ac
Michael Sherman <shermanm@uchicago.edu>
2026-08-07 18:20:13 +0000

    feat: toggle baremetal and vm features
    Toggle baremetal and VM customizations independently.

--- openstack_dashboard/dashboards/project/instances/tests.py
+++ openstack_dashboard/dashboards/project/instances/tests.py
@@ -3492,3 +3492,13 @@ class BaremetalRowActionTests(helpers.TestCase):
         names = self._row_action_names(self.flavors.first())
         # vm-only actions still in the list
         self.assertLessEqual({'resize', 'pause', 'lock'}, names)
+
+    # Flavor name matches, but the site does not apply CHI customizations.
+    @django.test.utils.override_settings(
+        CHAMELEON_BAREMETAL_FLAVOR_NAME='m1.tiny',
+        CHAMELEON_ENABLE_BAREMETAL=False)
+    @helpers.create_mocks(ROW_RENDER_MOCKS)
+    def test_nothing_hidden_when_site_has_no_baremetal(self):
+        names = self._row_action_names(self.flavors.first())
+        # vm-only actions still in the list
+        self.assertLessEqual({'resize', 'pause', 'lock'}, names)
