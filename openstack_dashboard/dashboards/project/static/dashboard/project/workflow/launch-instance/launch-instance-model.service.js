91c7301d95c499e0ca0bc36c332c4eaaff0b10f8
Michael Sherman <shermanm@uchicago.edu>
2026-08-14 16:25:41 +0000

    Don't require reservation hint when not baremetal
    Only baremetal uses the physical:host scheduler hint, don't require
    it in the wizard for VMs. This also lets us skip fetching reservations
    from blazar, allowing the wizard to work on sites without blazar (aka
    upstream)

--- openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-model.service.js
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-model.service.js
@@ -258,9 +258,8 @@
             model.newInstanceSpec.default_user_data = response;
           });
 
-        promise = $q.all([
+        var initTasks = [
           launchInstanceDefaults.then(setDefaultValues, noop),
-          blazarAPI.reservations().then(onGetReservations),
           novaAPI.getAvailabilityZones().then(onGetAvailabilityZones)
             .finally(onGetAvailabilityZonesComplete),
           novaAPI.getFlavors({
@@ -273,7 +272,14 @@
           serviceCatalog.ifTypeEnabled('network').then(getNetworks, noop),
           launchInstanceDefaults.then(addImageSourcesIfEnabled, noop),
           launchInstanceDefaults.then(addVolumeSourcesIfEnabled, noop)
-        ]);
+        ];
+
+        // Require reservation fetch only for baremetal
+        if (model.isBaremetal) {
+          initTasks.push(blazarAPI.reservations().then(onGetReservations));
+        }
+
+        promise = $q.all(initTasks);
 
         promise.then(onInitSuccess, onInitFail);
       }
@@ -442,8 +448,13 @@
     // django form handler doesn't look at scheduler hints, but the api handler
     // (openstack_dashboard/api/rest/nova.py#Servers.post) does
     function setFinalSpecReservation(finalSpec) {
-      finalSpec.scheduler_hints['reservation'] = finalSpec['reservation'];
-      delete finalSpec['reservation'];
+      // Do not require selected reservation hint if not baremetal
+      if (!model.isBaremetal) {
+        delete finalSpec.reservation;
+        return;
+      }
+      finalSpec.scheduler_hints.reservation = finalSpec.reservation;
+      delete finalSpec.reservation;
     }
 
     // Flavors
